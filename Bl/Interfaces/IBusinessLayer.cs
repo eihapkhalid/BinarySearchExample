64e461c76fd81633045cb49cf010373ce6f5c99e
eihap <eihapinfo@gmail.com>
2023-05-15 16:45:43 +0300

    update

---
+++ Bl/Interfaces/IBusinessLayer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bl.Interfaces
+{
+    public class IBusinessLayer<T>
+    {
+        int BinarySearch(int[] arr, int target);
+    }
+}
