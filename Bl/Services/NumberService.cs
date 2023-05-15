cbbd58956f97bfefbb4ca2be9e64e98be5e7f5a9
Eihap Khalid <Eihapinfo@gmail.com>
2023-05-15 17:32:41 +0300

    create BinarySearch and update refrnces bettwen  class lib

---
+++ Bl/Services/NumberService.cs
@@ -0,0 +1,40 @@
+﻿using Bl.Interfaces;
+using Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bl.Services
+{
+    public class NumberService : IBusinessLayer<TbNumber>
+    {
+        int IBusinessLayer<TbNumber>.BinarySearch(int[] arr, int target)
+        {
+
+            int left = 0;
+            int right = arr.Length - 1;
+
+            while (left <= right)
+            {
+                int mid = (left + right) / 2;
+
+                if (arr[mid] == target)
+                {
+                    return mid; // تم العثور على التطابق، يتم إرجاع المؤشر (الفهرس) للعنصر
+                }
+                else if (arr[mid] < target)
+                {
+                    left = mid + 1; // العنصر المطلوب أكبر من العنصر الموجود في النصف الأوسط، لذا نتحقق من الجزء الأيمن من المصفوفة
+                }
+                else
+                {
+                    right = mid - 1; // العنصر المطلوب أصغر من العنصر الموجود في النصف الأوسط، لذا نتحقق من الجزء الأيسر من المصفوفة
+                }
+            }
+
+            return -1; // لم يتم العثور على التطابق، يتم إرجاع قيمة -1
+        }
+    }
+}
