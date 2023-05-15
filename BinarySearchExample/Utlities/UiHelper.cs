6634fd20deed92549aef663cd6275ec44b071cd9
Eihap Khalid <Eihapinfo@gmail.com>
2023-05-15 17:40:14 +0300

    update uihelper

--- BinarySearchExample/Utlities/UiHelper.cs
+++ BinarySearchExample/Utlities/UiHelper.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Bl.Interfaces;
+using Domains;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,15 @@ namespace BinarySearchExample.Utlities
 {
     public class UiHelper
     {
+        #region dependency injection
+        IBusinessLayer<TbNumber> oNumberService;
+        TbNumber oTbNumber;
+        public UiHelper(IBusinessLayer<TbNumber> numberService, TbNumber tbNumber)
+        {
+            oNumberService = numberService;
+            oTbNumber = tbNumber;
+
+        }
+        #endregion
     }
 }
